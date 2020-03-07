0f474211ed4d1d23579127d93f86e1fc71543dfe
Javier Herrera <muchorrollodejavi@gmail.com>
2020-03-07 04:00:25 +0000

    it worksneeds a lot of work, but it read from the api and send back the data to telegram as requested

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,100 @@
-use teloxide::prelude::*;
-use std::collections::HashMap;
+use teloxide::{prelude::*, utils::command::BotCommand};
+use serde::{Deserialize};
+use tokio::task::block_in_place;
+//use serde_json::Result;
+
+#[derive(Deserialize, Debug)]
+struct Party {
+    id: String,
+    nameParty: String,
+    dateStart: String,
+    dateEnd: String,
+    nameType: String,
+    nameCountry: String,
+    isoCountry: String,
+    nameTown: String,
+    geoLat: Option<String>,
+    geoLon: Option<String>,
+    nameOrganizer: String,
+    urlOrganizer: Option<String>,
+    urlImageSmall: Option<String>,
+    urlImageMedium: Option<String>,
+    urlImageFull: Option<String>,
+    dateCreated: String,
+    dateModified: String,
+    nameStatus: String,
+    urlPartyHtml: String,
+    urlPartyJson: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Parties {
+    partylist: Vec<Party>,
+}
+
+#[derive(BotCommand)]
+#[command(rename = "lowercase", description = "These commands are supported:")]
+enum Command {
+    #[command(description = "display this text.")]
+    Help,
+    #[command(description = "Check if the bot is alive.")]
+    Ping,
+    #[command(description = "Return a list with the upcoming raves.")]
+    Raves,
+}
+
+
+async fn answer(
+    cx: DispatcherHandlerCx<Message>,
+    command: Command,
+) -> ResponseResult<()> {
+    match command {
+        Command::Help => cx.answer(Command::descriptions()).send().await?,
+        Command::Ping => cx.answer("Pong!").send().await?,
+        Command::Raves => cx.answer(raves().await).send().await?,
+    };
+
+    Ok(())
+}
+
+async fn handle_commands(rx: DispatcherHandlerRx<Message>) {
+    // Only iterate through commands in a proper format:
+    rx.commands::<Command, String>(format!("Goabasebot"))
+        // Execute all incoming commands concurrently:
+        .for_each_concurrent(None, |(cx, command, _)| async move {
+            answer(cx, command).await.log_on_error().await;
+        })
+        .await;
+}
+
+async fn raves() -> String {
+    let data = block_in_place(|| get_parties());
+    //TODO quitar unwrap
+    let parsed: Parties = serde_json::from_str(&data.unwrap()).unwrap();
+    //TODO Esto es una chapuza, mejorarlo
+    let ref name = parsed.partylist[0].nameParty;
+    return name.to_string();
+
+}
 
 #[tokio::main]
 async fn main() {
-    println!("Starting!");
-    let data = get_parties().await;
-    
-    println!("{:?}", data);
+    run().await;
 
+}
+
+async fn run() {
     teloxide::enable_logging!();
-    log::info!("Starting ping_pong_bot!");
-
-    let data = get_parties().await;
+    //log::info!("Starting simple_commands_bot!");
 
     let bot = Bot::from_env();
 
-    Dispatcher::new(bot)
-        .messages_handler(|rx: DispatcherHandlerRx<Message>| {
-            rx.for_each(|message| async move {
-                message.answer("pong").send().await.log_on_error().await;
-            })
-        })
-        .dispatch()
-        .await;
-    
+    Dispatcher::new(bot).messages_handler(handle_commands).dispatch().await;
 }
 
-async fn get_parties() -> Result<String, reqwest::Error> {
-    let resp = reqwest::get("https://www.rust-lang.org").await?.text().await?;
+fn get_parties() -> Result<String, reqwest::Error> {
+    let resp = reqwest::blocking::get("https://www.goabase.net/api/party/json/?country=ES")?
+        .text()?;
 
     Ok((resp))
 }
